88180751d796d38468ffc2fd473da8b570ac701c
liuxiaokun <liuxk@cloudoer.com>
2018-09-04 14:34:18 +0800

    add logback configuration, and format the pattern

--- src/main/java/com/cloudoer/project/project/module/controller/TestController.java
+++ src/main/java/com/cloudoer/project/project/module/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.cloudoer.project.project.module.controller;
 
 import com.cloudoer.project.project.module.config.UserConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Date;
  * @since 2018/9/4
  */
 @RestController
+@Slf4j
 public class TestController {
 
     @Autowired
@@ -25,6 +27,7 @@ public class TestController {
 
     @GetMapping("config")
     public Object testConfig() {
+        log.error(userConfig.toString());
         return userConfig;
     }
 
