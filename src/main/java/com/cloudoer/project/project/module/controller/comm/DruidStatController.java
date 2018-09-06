69f821f82d42b92c6750a7441ecdd416e03ad227
liuxiaokun <liuxk@cloudoer.com>
2018-09-06 13:38:04 +0800

    add druid connections pool stat function

---
+++ src/main/java/com/cloudoer/project/project/module/controller/comm/DruidStatController.java
@@ -0,0 +1,20 @@
+package com.cloudoer.project.project.module.controller.comm;
+
+import com.alibaba.druid.stat.DruidStatManagerFacade;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuxiaokun
+ * @version 0.0.1
+ * @since 2018/9/6
+ */
+@RestController
+public class DruidStatController {
+
+    @GetMapping("/druid/stat")
+    public Object druidStat() {
+        // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据，除此之外 DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
+        return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+    }
+}
